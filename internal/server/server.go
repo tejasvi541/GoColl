dec7eef844239e500af745f5576cc0fcab2824a7
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-09-04 21:37:17 -0400

    Imported Dependcies and created welcom handler

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,6 +3,15 @@ package server
 import (
 	"flag"
 	"os"
+	"time"
+
+	"github.com/gofiber/fiber/middleware/logger"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
+	"golang.org/x/net/websocket"
 )
 
 // Flag values
@@ -21,10 +30,20 @@ func Run() error {
 		*address = ":8080"
 	}
 
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Use(logger.New())
+	app.Use(cors.New())
+
 	app.Get("/", handlers.Welcome)
 	app.Get("/room/create", handlers.RoomCreate)
 	app.Get("/room/:uuid", handlers.Room)
-	app.Get("/room/:uuid/websocket",)
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWebsocket, websocket.Config{
+		HandshakeTimeout: 10 * time.Second,
+		}))
 	app.Get("/room/:uuid/chat", handlers.RoomChat)
 	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
 	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
